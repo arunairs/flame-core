ff10706a7624edf316bac078906304a48772281c
Eric Zhang <jiaan.zhang@oracle.com>
2016-09-27 00:39:15 +0800

    扩展结构

---
+++ buildSrc/src/main/groovy/cn/blinkmind/promise/buildsrc/TaskGroup.groovy
@@ -0,0 +1,10 @@
+package cn.blinkmind.promise.buildsrc
+
+/**
+ * @author jiaan.zhang@oracle.com
+ * @date 27/09/2016 12:22 AM
+ */
+class TaskGroup {
+    static final String SERVER = 'Server';
+    static final String TEST = 'Test';
+}
