414f375f6c3b9dd58aba4288bfdf8c2fd96a5dfb
Eric Zhang <jiaan.zhang@oracle.com>
2016-09-27 00:06:12 +0800

    添加druid data source

--- server/api/src/main/java/cn/blinkmind/promise/server/Application.java
+++ server/api/src/main/java/cn/blinkmind/promise/server/Application.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author jiaan.zhang@oracle.com
  * @date 9/14/16 1:48 PM
  */
 @SpringBootApplication
+@EnableTransactionManagement
 @EnableAspectJAutoProxy
 @EnableAsync
 public class Application
