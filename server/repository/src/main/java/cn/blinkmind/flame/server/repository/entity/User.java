0cbcacc46fccbf81bd729ef044f5fdba509c2d08
Eric Zhang <jiaan.zhang@outlook.com>
2017-01-07 00:56:40 +0800

    修复bug

--- server/repository/src/main/java/cn/blinkmind/flame/server/repository/entity/User.java
+++ server/repository/src/main/java/cn/blinkmind/flame/server/repository/entity/User.java
@@ -1,6 +1,6 @@
 package cn.blinkmind.flame.server.repository.entity;
 
-import java.beans.Transient;
+import org.springframework.data.annotation.Transient;
 
 @org.springframework.data.mongodb.core.mapping.Document(collection = "users")
 public class User extends Account
