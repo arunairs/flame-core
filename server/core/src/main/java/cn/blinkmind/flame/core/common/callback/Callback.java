03e10199f756c98b676447c58bf897c60a153968
Eric Zhang <jiaan.zhang@outlook.com>
2017-03-01 23:20:13 +0800

    移除callback包

--- server/core/src/main/java/cn/blinkmind/flame/core/common/callback/Callback.java
+++
@@ -1,7 +0,0 @@
-package cn.blinkmind.flame.core.common.callback;
-
-import java.util.function.Supplier;
-
-public interface Callback<T, E> {
-    <R> CallbackStream<T, R, E> map(Supplier<R> supplier);
-}
