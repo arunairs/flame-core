c92c311e40683fe21ee07dcc0bd4536710a34c6a
Eric Zhang <jiaan.zhang@outlook.com>
2017-03-17 23:09:23 +0800

    更新Headers

--- server/repository/src/main/java/cn/blinkmind/flame/repository/model/Headers.java
+++ server/repository/src/main/java/cn/blinkmind/flame/repository/model/Headers.java
@@ -1,38 +1,61 @@
 package cn.blinkmind.flame.repository.model;
 
+import com.google.common.collect.Maps;
 import lombok.ToString;
 
 import java.util.HashMap;
 
 @ToString
-public class Headers extends HashMap<String, Object> {
+public class Headers {
+    private HashMap<String, Object> hashMap = Maps.newHashMap();
+
     public Boolean getBoolean(String key) {
-        return this.getHeaderOrDefault(key, null);
+        return this.getBoolean(key, null);
+    }
+
+    public Boolean getBoolean(String key, Boolean defaultValue) {
+        return this.getOrDefault(key, defaultValue);
     }
 
     public Integer getInteger(String key) {
-        return this.getHeaderOrDefault(key, null);
+        return this.getInteger(key, null);
+    }
+
+    public Integer getInteger(String key, Integer defaultValue) {
+        return this.getOrDefault(key, defaultValue);
     }
 
     public Long getLong(String key) {
-        return this.getHeaderOrDefault(key, null);
+        return this.getLong(key, null);
     }
 
-    public Long getString(String key) {
-        return this.getHeaderOrDefault(key, null);
+    public Long getLong(String key, Long defaultValue) {
+        return this.getOrDefault(key, defaultValue);
     }
 
-    public <T> T getHeader(String key) {
-        return this.getHeaderOrDefault(key, null);
+    public String getString(String key) {
+        return this.getString(key, null);
     }
 
-    public Headers add(String key, Object value) {
-        super.put(key, value);
+    public String getString(String key, String defaultValue) {
+        return this.getOrDefault(key, defaultValue);
+    }
+
+    public <T> T get(String key) {
+        return this.get(key, null);
+    }
+
+    public <T> T get(String key, T defaultValue) {
+        return this.getOrDefault(key, defaultValue);
+    }
+
+    public <T> Headers put(String key, T value) {
+        this.hashMap.put(key, value);
         return this;
     }
 
     @SuppressWarnings("unchecked")
-    public <T> T getHeaderOrDefault(String key, T defaultValue) {
-        return (T) super.getOrDefault(key, defaultValue);
+    private <T> T getOrDefault(String key, T defaultValue) {
+        return (T) this.hashMap.getOrDefault(key, defaultValue);
     }
 }
