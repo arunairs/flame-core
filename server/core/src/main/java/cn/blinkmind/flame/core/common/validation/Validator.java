46c8c91b0498bd8c53837d304407128964424952
Eric Zhang <jiaan.zhang@outlook.com>
2017-03-19 13:45:31 +0800

    更新validator

--- server/core/src/main/java/cn/blinkmind/flame/core/common/validation/Validator.java
+++ server/core/src/main/java/cn/blinkmind/flame/core/common/validation/Validator.java
@@ -1,9 +1,11 @@
 package cn.blinkmind.flame.core.common.validation;
 
+import java.util.function.Supplier;
+
 public class Validator<T> {
-    private T actual;
-    private Matcher<T> matcher;
-    private Runnable otherwise;
+    private final T actual;
+    private final Matcher<T> matcher;
+    private final Runnable otherwise;
 
     private Validator(T actual, Matcher<T> matcher, Runnable otherwise) {
         this.actual = actual;
@@ -11,11 +13,11 @@ public class Validator<T> {
         this.otherwise = otherwise;
     }
 
-    public <E> Validator<E> and(E actual, Matcher<E> matcher, Runnable otherwise) {
+    public <E> Validator<E> and(final E actual, final Matcher<E> matcher, final Runnable otherwise) {
         return Validator.validateThat(actual, matcher, otherwise);
     }
 
-    public Validator<Boolean> and(Boolean condition, Runnable otherwise) {
+    public Validator<Boolean> and(final Boolean condition, final Runnable otherwise) {
         return Validator.validateThat(condition, otherwise);
     }
 
@@ -27,6 +29,20 @@ public class Validator<T> {
         Validator<T> validator = new Validator<>(actual, matcher, otherwise);
         validator.match();
         return validator;
+    }
+
+    public static Runnable orElseThrow(final Supplier<RuntimeException> supplier) {
+        return orElseThrow(supplier.get());
+    }
+
+    public static Runnable orElseThrow(final RuntimeException exception) {
+        return orElse(() -> {
+            throw exception;
+        });
+    }
+
+    public static Runnable orElse(final Runnable runnable) {
+        return runnable;
     }
 
     private void match() {
