df31a45d50c07837e9ae97ef6f5a1da5b8ec5282
Eric Zhang <jiaan.zhang@outlook.com>
2017-04-28 00:44:01 +0800

    优化异常处理

--- server/core/src/main/java/io/bayberry/core/controller/advice/ExceptionHandlerAdvice.java
+++ server/core/src/main/java/io/bayberry/core/controller/advice/ExceptionHandlerAdvice.java
@@ -1,25 +1,23 @@
 package io.bayberry.core.controller.advice;
 
 import io.bayberry.core.exception.BadRequestException;
-import io.bayberry.core.exception.Error;
-import io.bayberry.core.exception.Errors;
+import io.bayberry.core.common.Error;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ExceptionHandlerAdvice
-{
+public class ExceptionHandlerAdvice {
+
     @ExceptionHandler(value = BadRequestException.class)
-    private ResponseEntity<Error> handleBadRequestException(BadRequestException exception)
-    {
-        return ResponseEntity.status(exception.getError().getHttpStatus()).body(exception.getError());
+    private ResponseEntity<Error> handleBadRequestException(BadRequestException exception) {
+        return ResponseEntity.status(exception.getHttpStatus()).body(exception.getError());
     }
 
     @ExceptionHandler(value = DuplicateKeyException.class)
-    private ResponseEntity<Error> handleDuplicateKeyException(DuplicateKeyException exception)
-    {
-        return ResponseEntity.status(Errors.RESOURCE_ALREADY_EXISTS.getError().getHttpStatus()).body(Errors.RESOURCE_ALREADY_EXISTS.getError());
+    private ResponseEntity<Error> handleDuplicateKeyException(DuplicateKeyException exception) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(Error.RESOURCE_ALREADY_EXISTS);
     }
 }
