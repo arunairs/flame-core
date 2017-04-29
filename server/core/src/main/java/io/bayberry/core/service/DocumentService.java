0effc46eb13074dd7e09a4d7388d0066e422bd5b
Eric Zhang <jiaan.zhang@outlook.com>
2017-04-29 14:17:47 +0800

    refactor

--- server/core/src/main/java/io/bayberry/core/service/DocumentService.java
+++ server/core/src/main/java/io/bayberry/core/service/DocumentService.java
@@ -1,10 +1,10 @@
 package io.bayberry.core.service;
 
 import io.bayberry.core.authentication.User;
+import io.bayberry.core.common.Error;
 import io.bayberry.core.common.Result;
 import io.bayberry.core.event.DocumentCreatedEvent;
 import io.bayberry.core.event.EventPublisher;
-import io.bayberry.core.common.Error;
 import io.bayberry.repository.DocumentRepository;
 import io.bayberry.repository.model.Document;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,15 +22,15 @@ public class DocumentService {
         this.eventPublisher = eventPublisher;
     }
 
-    public Result<Document, Error> get(Long id, User user) {
-        Document document = this.documentRepository.get(id);
-        return Result.failIfNull(document, Error.DOCUMENT_NOT_FOUND);
-    }
-
     public Result<Document, Error> create(Document document, User user) {
         document.setCreatorId(user.getId());
         documentRepository.insert(document);
         eventPublisher.publish(new DocumentCreatedEvent(document));
         return Result.ok(document);
+    }
+
+    public Result<Document, Error> get(Long id, User user) {
+        Document document = this.documentRepository.get(id);
+        return Result.failIfNull(document, Error.DOCUMENT_NOT_FOUND);
     }
 }
