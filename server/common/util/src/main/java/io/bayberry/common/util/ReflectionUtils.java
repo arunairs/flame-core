702dd8e1ddc884e7faacf6066d2b960231ee346d
Eric Zhang <jiaan.zhang@outlook.com>
2017-04-30 17:38:04 +0800

    refactor ReflectionUtils

--- server/common/util/src/main/java/io/bayberry/common/util/ReflectionUtils.java
+++ server/common/util/src/main/java/io/bayberry/common/util/ReflectionUtils.java
@@ -7,8 +7,14 @@ import java.util.Set;
 
 public class ReflectionUtils {
 
-    public static Set<Class<?>> getClassesAnnotatedWith(final String packageName, final Annotation annotation) {
-        return getReflections(packageName).getTypesAnnotatedWith(annotation);
+    public static Set<Class<?>> getClassesAnnotatedWith(final Class<?> basePackageClass,
+                                                        final Class<? extends Annotation> annotationClass) {
+        return getClassesAnnotatedWith(basePackageClass.getPackage().getName(), annotationClass);
+    }
+
+    public static Set<Class<?>> getClassesAnnotatedWith(final String packageName,
+                                                        final Class<? extends Annotation> annotationClass) {
+        return getReflections(packageName).getTypesAnnotatedWith(annotationClass);
     }
 
     private static Reflections getReflections(final String packageName) {
