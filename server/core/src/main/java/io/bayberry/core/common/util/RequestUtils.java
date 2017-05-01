e8edf424b8e080268fd1cd654869934d4950011b
Eric Zhang <jiaan.zhang@outlook.com>
2017-05-01 22:38:02 +0800

    add ApiRequestBodyAdvice

---
+++ server/core/src/main/java/io/bayberry/core/common/util/RequestUtils.java
@@ -0,0 +1,13 @@
+package io.bayberry.core.common.util;
+
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+public class RequestUtils {
+
+    public static Map<String, String> getPathVariables(HttpServletRequest request) {
+        return (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+    }
+}
