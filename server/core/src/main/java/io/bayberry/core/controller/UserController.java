ef6bfc15546270349fa31c1cb9ffc50c2f79d7d3
Eric Zhang <jiaan.zhang@outlook.com>
2017-05-09 23:15:03 +0800

    refactor

--- server/core/src/main/java/io/bayberry/core/controller/UserController.java
+++ server/core/src/main/java/io/bayberry/core/controller/UserController.java
@@ -1,7 +1,7 @@
 package io.bayberry.core.controller;
 
 import io.bayberry.core.service.UserService;
-import io.bayberry.repository.model.User;
+import io.bayberry.repository.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
