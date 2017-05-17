fb8cf7e1290ae7f59f40d7a6bbc3799e25ff930e
Eric Zhang <jiaan.zhang@outlook.com>
2017-05-18 00:15:11 +0800

    update

--- server/core/src/main/java/io/bayberry/core/Bootstrap.java
+++ server/core/src/main/java/io/bayberry/core/Bootstrap.java
@@ -1,13 +1,11 @@
 package io.bayberry.core;
 
-import io.bayberry.repository.AbstractMongoRepository;
+import io.bayberry.core.repository.AbstractMongoRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = {Bootstrap.class, AbstractMongoRepository.class})
 @EnableMongoRepositories(basePackageClasses = {AbstractMongoRepository.class})
 public class Bootstrap {
     public static void main(String[] args) {
