14764f7fe05c2afbc0dfee538b12177f343ce8fe
Pojo Zhang <pojo.zhang@gmail.com>
2017-07-23 19:59:15 +0800

    use junit

--- server/core/src/test/java/io/bayberry/core/service/ArchiveServiceTest.java
+++ server/core/src/test/java/io/bayberry/core/service/ArchiveServiceTest.java
@@ -1,13 +1,17 @@
 package io.bayberry.core.service;
 
 import io.bayberry.core.SpringBootTestNGContext;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.testng.annotations.Test;
+import org.springframework.context.ApplicationContext;
 
 public class ArchiveServiceTest extends SpringBootTestNGContext {
 
     @Autowired
     private ArchiveService archiveService;
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Test
     public void testDiff() throws Exception {
